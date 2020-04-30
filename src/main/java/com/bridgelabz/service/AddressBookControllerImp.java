77bb81dab7bfd525d58f3c99a2125ba037e31991
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-30 17:42:55 +0530

    ADDED: Method to update person data

--- src/main/java/com/bridgelabz/service/AddressBookControllerImp.java
+++ src/main/java/com/bridgelabz/service/AddressBookControllerImp.java
@@ -10,17 +10,44 @@ import java.util.ArrayList;
 public class AddressBookControllerImp implements IAddressBook {
     ObjectMapper objectMapper = new ObjectMapper();
 
+    public ArrayList<PersonInformation> readFileData(String filePath) throws IOException {
+        return objectMapper.readValue(new File(filePath), new TypeReference<ArrayList<PersonInformation>>() {
+        });
+    }
+
+    public void writeFileData(ArrayList<PersonInformation> list, String filePath) throws IOException {
+        objectMapper.writeValue(new File(filePath), list);
+    }
+
     @Override
     public void addPersonInformation(PersonInformation personInformation, String filePath) {
         try {
             //Read data
-            ArrayList<PersonInformation> data = objectMapper.readValue
-                    (new File(filePath), new TypeReference<ArrayList<PersonInformation>>() {
-                    });
+            ArrayList<PersonInformation> data = readFileData(filePath);
             data.add(personInformation);
             //Write data
-            objectMapper.writeValue(new File(filePath), data);
+            writeFileData(data, filePath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
+    @Override
+    public void updatePersonData(PersonInformation personInformation, String filePath, String uniqueData) {
+        try {
+            ArrayList<PersonInformation> data = readFileData(filePath);
+            for (PersonInformation personData : data) {
+                if (personData.getPhoneNumber().equals(uniqueData)) {
+                    personData.setFirstName(personInformation.getFirstName());
+                    personData.setLastName(personInformation.getLastName());
+                    personData.setAddress(personInformation.getAddress());
+                    personData.setCity(personInformation.getCity());
+                    personData.setState(personInformation.getState());
+                    personData.setZip(personInformation.getZip());
+                    personData.setPhoneNumber(personInformation.getPhoneNumber());
+                }
+            }
+            writeFileData(data, filePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/main/java/com/bridgelabz/service/IAddressBook.java
+++ src/main/java/com/bridgelabz/service/IAddressBook.java
@@ -4,4 +4,6 @@ import com.bridgelabz.model.PersonInformation;
 
 public interface IAddressBook {
     public void addPersonInformation(PersonInformation personInformation, String filePath);
+
+    public void updatePersonData(PersonInformation personInformation,String filePath,String uniqueData);
 }
