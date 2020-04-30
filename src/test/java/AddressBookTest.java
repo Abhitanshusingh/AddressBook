8a0b108da0d5240db9a83507d4df691ca47ad452
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-05-01 00:01:11 +0530

    ADDED: Test case to check data is stored in sortedorder

--- src/test/java/AddressBookTest.java
+++ src/test/java/AddressBookTest.java
@@ -27,13 +27,12 @@ public class AddressBookTest {
     @Test
     public void givenPersonalInformation_whenStoreInFile_shouldReturnTrue() {
         try {
-            PersonInformation personInformation = new PersonInformation
-                    ("Abhitanshu", "Baghel", "TT nagar", "Shahdol", "Madhya Pradesh", "484110", "7000458100");
+            PersonInformation personInformation = new PersonInformation("Abhitanshu", "Baghel", "TT nagar", "Shahdol", "Madhya Pradesh", "484110", "7000458100");
             addressBookControllerImp.addPersonInformation(personInformation, filePath);
             ArrayList<PersonInformation> data = objectMapper
                     .readValue(new File(filePath), new TypeReference<ArrayList<PersonInformation>>() {
                     });
-            Assert.assertEquals(personInformation.getPhoneNumber(), data.get(data.size()-1).getPhoneNumber());
+            Assert.assertEquals(personInformation.getPhoneNumber(), data.get(data.size() - 1).getPhoneNumber());
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -43,8 +42,7 @@ public class AddressBookTest {
     public void givenPersonalInformation_whenUpdateData_shouldReturnTrue() {
         try {
             String uniqueData = "7000458100";
-            PersonInformation personInformation = new PersonInformation
-                    ("Laksh", "Mehta", "BTM 2nd stage", "Bangalore", "Karnataka", "746061", "7134329292");
+            PersonInformation personInformation = new PersonInformation("Laksh", "Mehta", "BTM 2nd stage", "Bangalore", "Karnataka", "746061", "7134329292");
             int indexNumber = addressBookControllerImp.updatePersonData(personInformation, filePath, uniqueData);
             ArrayList<PersonInformation> data = objectMapper
                     .readValue(new File(filePath), new TypeReference<ArrayList<PersonInformation>>() {
@@ -61,12 +59,30 @@ public class AddressBookTest {
         ArrayList<PersonInformation> beforeDeletedData = objectMapper
                 .readValue(new File(filePath), new TypeReference<ArrayList<PersonInformation>>() {
                 });
-        int beforeDeletedSize = beforeDeletedData.size() ;
-        addressBookControllerImp.deletePersonData(personInformation, filePath, uniqueData);
+        int beforeDeletedSize = beforeDeletedData.size();
+        addressBookControllerImp.deletePersonData(filePath, uniqueData);
         ArrayList<PersonInformation> afterDeletedData = objectMapper
                 .readValue(new File(filePath), new TypeReference<ArrayList<PersonInformation>>() {
                 });
-        int afterDeletedSize = afterDeletedData.size() +1 ;
-        Assert.assertEquals(beforeDeletedSize,afterDeletedSize);
+        int afterDeletedSize = afterDeletedData.size() + 1;
+        Assert.assertEquals(beforeDeletedSize, afterDeletedSize);
+    }
+
+    @Test
+    public void givenPersonInformation_whenSortedData_shouldReturnTrue() throws IOException {
+        PersonInformation personInformation1 = new PersonInformation("Bhanu", "Baba", "BTM 2nd stage", "Bangalore", "Karnataka", "746061", "7134329292");
+        PersonInformation personInformation2 = new PersonInformation("Modi", "Bali", "BTM 2nd stage", "Bangalore", "Karnataka", "746061", "7134329292");
+        PersonInformation personInformation3 = new PersonInformation("Aayush", "sahu", "BTM 2nd stage", "Bangalore", "Karnataka", "746061", "7134329292");
+
+        addressBookControllerImp.addPersonInformation(personInformation1, filePath);
+        addressBookControllerImp.addPersonInformation(personInformation2, filePath);
+        addressBookControllerImp.addPersonInformation(personInformation3, filePath);
+        addressBookControllerImp.sortPersonData(filePath);
+
+        ArrayList<PersonInformation> data = objectMapper
+                .readValue(new File(filePath), new TypeReference<ArrayList<PersonInformation>>() {
+                });
+        Assert.assertEquals("Aayush", data.get(0).getFirstName());
+        Assert.assertEquals("Modi", data.get(data.size() - 1).getFirstName());
     }
 }
