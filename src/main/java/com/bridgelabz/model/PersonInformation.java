c296b4d479b336089b5875366d993e7bec1cf02e
Abhitanshusingh <Abhitanshusingh99@gmail.com>
2020-04-30 13:06:21 +0530

    ADDED: POJO class to store address book data

---
+++ src/main/java/com/bridgelabz/model/PersonInformation.java
@@ -0,0 +1,84 @@
+package com.bridgelabz.model;
+
+public class PersonInformation {
+    private String firstName;
+    private String lastName;
+    private String state;
+    private long zip;
+    private String city;
+    private long phoneNumber;
+
+    public PersonInformation() {
+
+    }
+
+    public PersonInformation(String firstName, String lastName, String city, String state, long zip, long phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public long getZip() {
+        return zip;
+    }
+
+    public void setZip(long zip) {
+        this.zip = zip;
+    }
+
+    public long getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonInformation{" +
+                "state='" + state + '\'' +
+                ", zip=" + zip +
+                ", city='" + city + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", phoneNumber=" + phoneNumber +
+                '}';
+    }
+}
